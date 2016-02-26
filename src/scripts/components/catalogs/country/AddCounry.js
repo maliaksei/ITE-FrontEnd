166020bb16b9c7cf5c54954feaccd402afb9ed3a
Milko, Aleksey <a.milko@itransition.com>
2016-02-26 18:27:10 +0300

    Added react-bootstrap-multiselect and Input

--- src/scripts/components/catalogs/country/AddCounry.js
+++ src/scripts/components/catalogs/country/AddCounry.js
@@ -2,15 +2,48 @@
  * Created by a.milko on 25.02.2016.
  */
 import React,{Component} from 'react';
+import {Input} from 'react-bootstrap';
+import Multiselect from 'react-bootstrap-multiselect';
 
 class AddCounry extends Component {
-
     render() {
         return (
-            <h1 className="title">IndexCatalog</h1>
+            <div>
+                <div className="form-horizontal">
+                    <Input
+                        type="text"
+                        name="firstname"
+                        placeholder="First Name"
+                        required="required"
+                        label={ <span>First Name <span className="text-danger small">*</span></span> }
+                        labelClassName="col-sm-2"
+                        wrapperClassName="col-sm-4"
+                        hasFeedback
+                        feedbackIcon={<i className="form-control-feedback fa fa-pencil" key="icon" />} />
+                    <Input
+                        type="text"
+                        name="firstname"
+                        placeholder="First Name"
+                        required="required"
+                        label={ <span>First Name <span className="text-danger small">*</span></span> }
+                        labelClassName="col-sm-2"
+                        wrapperClassName="col-sm-4"
+                        hasFeedback
+                        feedbackIcon={<i className="form-control-feedback fa fa-pencil" key="icon" />} />
+                    <Multiselect data={products} multiple />
+                </div>
+                <br/>
+            </div>
         );
     }
-
 }
+
+var products = [{
+    value: 'One',
+    selected:true
+},{
+    value: 'Two',
+    selected:false
+}];
 
 export default AddCounry;
--- src/scripts/components/catalogs/country/ListCountry.js
+++ src/scripts/components/catalogs/country/ListCountry.js
@@ -3,16 +3,26 @@
  */
 import React,{Component} from 'react';
 import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+import { Link } from 'react-router';
 
 class ListCountry extends Component {
 
     render() {
         return (
-            <BootstrapTable data={products} striped={true} hover={true}>
-                <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Country ID</TableHeaderColumn>
-                <TableHeaderColumn dataField="name" dataSort={true} dataAlign="center" >Country</TableHeaderColumn>
-                <TableHeaderColumn dataField="" dataSort={false} dataAlign="center" >Actions</TableHeaderColumn>
-            </BootstrapTable>
+            <div>
+                <div className="row">
+                    <Link to="/catalogs/addCountry" className="btn btn-rounded btn-default">Add</Link>
+                </div>
+                <br/>
+                <div className="row">
+                    <BootstrapTable data={products} striped={true} hover={true}>
+                        <TableHeaderColumn dataField="id" isKey={true} dataAlign="center" dataSort={true}>Country ID</TableHeaderColumn>
+                        <TableHeaderColumn dataField="name" dataSort={true} dataAlign="center" >Country</TableHeaderColumn>
+                        <TableHeaderColumn dataField="id" dataSort={false} dataAlign="center" dataFormat={actionFormatter}>Actions</TableHeaderColumn>
+                    </BootstrapTable>
+                </div>
+
+            </div>
         );
     }
 }
@@ -24,6 +34,9 @@ var products = [{
     name: "USA"
 }];
 
-
+function actionFormatter(cell, row){
+    return '<button type="button" class="btn btn-rounded btn-default btn-icon"><i class="fa fa-pencil"></i></button> ' +
+        '<button type="button" class="btn btn-rounded btn-danger btn-icon"><i class="fa fa-trash-o"></i></button>';
+}
 
 export default ListCountry;
